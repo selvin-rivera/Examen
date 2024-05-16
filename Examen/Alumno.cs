63124767a2d72355553c0364fc19886175b5f6f8
selvin-rivera <nrivera.clements@gmail.com>
2024-05-15 21:30:26 -0600

    creando clase padre

--- Examen/Alumno.cs
+++ Examen/Alumno.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Examen
 {
-    internal class Alumno
+    public class Alumno
     {
+        public string? NombreAlumno {  get; set; }
+        public  string? NumeroCuenta { get; set; }
+        public string? Email { get; set; } 
     }
 }
