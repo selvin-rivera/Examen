9f74d8654f0ddffa6daf8226b1f45476ff20220e
selvin-rivera <nrivera.clements@gmail.com>
2024-05-15 22:00:05 -0600

    creando interface
    llamada IAsignatura, dicha interfaz llevará los siguientes métodos: CalcularNotaFinal sin y con parametros, mensaje basado en la nota fina y imprimira los datos.

--- Examen/IAsignatura.cs
+++ Examen/IAsignatura.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Examen
 {
-    internal interface IAsignatura
+    public interface IAsignatura
     {
+        // <summary>
+        /// sumará las 3 notas parciales
+        /// </summary>
+        /// <returns>la nota final</returns>
+        public double CalcularNotaFinal();
+
+        // <summary>
+        /// sumará las 3 notas parciales a partir de los parámetros
+        /// </summary>
+        /// <returns>la nota final</returns>
+        public double CalcularNotaFinal(double nota1, double nota2, double nota3);
+
+        // <summary>
+        /// mostrará un mensaje basado en de la nota final
+        /// </summary>
+        /// <returns>el mensaje de acuerdo a la nota final</returns>
+        public string MensajeNotaFinal(double notaFinal);
+
+        // <summary>
+        /// mostrará en pantalla los datos del alumno, datos de la asignatura y la nota final 
+        /// </summary>
+        /// <returns>datos del alumno, datos de la asgnatura y la nota finall</returns>
+        public void Imprimir();
     }
 }
